24ad7af763081856940b17bc8cf826698b5dccb2
jdarshan5 <57329561+jdarshan5@users.noreply.github.com>
2020-11-26 16:32:22 +0530

    Delete admin.py

--- UserMoment/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserMoment, MomentTags, MomentHashtags
-
-# Register your models here.
-
-admin.site.register(UserMoment)
-admin.site.register(MomentTags)
-admin.site.register(MomentHashtags)
